c9706ea19c998d63518554789e83139a1110d4d8
FTBAngel <snapgod12@gmail.com>
2023-08-09 22:07:33 +1000

    added typehints

--- server/rediska.py
+++ server/rediska.py
@@ -1,18 +1,21 @@
+import redis
+
+
 class CacheRep:
-    def __init__(self, redis_loop):
+    def __init__(self, redis_loop: redis.Redis):
         self.redis = redis_loop
 
-    async def set(self, key, value, expire=3600):
+    async def set(self, key: str, value: str, expire=3600):
         await self.redis.set(key, value)
         await self.redis.expire(key, expire)
 
-    async def get(self, key):
+    async def get(self, key: str):
         return await self.redis.get(key)
 
-    async def delete(self, key):
+    async def delete(self, key: str):
         return await self.redis.delete(key)
 
-    async def exists(self, key):
+    async def exists(self, key: str):
         return await self.redis.exists(key)
 
     async def flush_db(self):
